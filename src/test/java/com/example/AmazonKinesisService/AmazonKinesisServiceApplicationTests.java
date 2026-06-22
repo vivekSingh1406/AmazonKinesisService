cb8d9434785574bf621513dfcb83a386bcfe76b9
vivek-singh <139918328+vivekSingh1406@users.noreply.github.com>
2026-06-22 22:41:53 +0530

    Add files via upload

---
+++ src/test/java/com/example/AmazonKinesisService/AmazonKinesisServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AmazonKinesisService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonKinesisServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
